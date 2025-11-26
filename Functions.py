0552141f1b7f6892afd63896b059a0123b64f617
shashi21082000 <shashikanttiwari314@gmail.com>
2025-11-26 15:54:12 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,63 @@
+# def User(a,b):
+#     print(a+b)
+    
+# User(2,3)
+
+# def calc_highest(a,b,c):
+#     if a>b and a>c:
+#         print(f"Highest number is, {a}")
+#     elif b>a and b>c:
+#         print(f"Highest number is, {b}")
+#     else:
+#         print(f"Highest number is, {c}")
+        
+# calc_highest(23,34,67)
+
+# def cel_to_far():
+#     f = (c * 9/5) + 32
+#     print("Temperature in Fahrenheit:", f)
+
+# c = int(input("Enter in celcius"))
+# cel_to_far()
+
+# def sum(n):
+#     if n==0:
+#         return 0
+#     else:
+#         return(n + sum(n-1))
+    
+# print(sum(5))
+    
+
+# def Star(n):
+#     for i in range(n,0,-1):
+#         print("*"*i)
+        
+        
+# Star(5)
+
+# def star(n):
+#     if n==0:
+#         return 
+#     else:
+#         print("*"*n)
+#         star(n-1)
+# star(5)
+
+# def i_to_c():
+#     c = 2.54 * n 
+#     print(c)
+    
+# n = int(input("Enter the number"))
+# i_to_c() 
+
+
+# def remove(List, word):
+#     for item in List:
+#        List.remove(word)
+#        return List
+    
+# List = ["Rohan","Man","Sohan","Mohan","Ram","an"]
+# print(remove(List, "an"))
+     
+    
